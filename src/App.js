c7d0c3749ce0a1dd2c9e8502236a89c9d1b2d260
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-07-06 18:36:02 -0500

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      <footer>
+        <a
+          href="https://github.com/Winx1718/react-weather-too.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source project
+        </a>
+        , by{" "}
+        <a
+          href="https://lustrous-gelato-606801.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Layna Moore
+        </a>
+      </footer>
+    </div>
+  );
 }
