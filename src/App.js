ac65dc41c0639c3a40da5a8f5439f70a6a3c2593
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-07-07 15:28:45 -0500

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        <a
-          href="https://github.com/Winx1718/react-weather-too.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source project
-        </a>
-        , by{" "}
-        <a
-          href="https://lustrous-gelato-606801.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Layna Moore
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App TOO</h1>
+        <footer>
+          <a
+            href="https://github.com/Winx1718/react-weather-too.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source project
+          </a>
+          , by{" "}
+          <a
+            href="https://lustrous-gelato-606801.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Layna Moore
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
